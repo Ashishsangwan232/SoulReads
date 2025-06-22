fe8952487ed586ddebd53ed6008ded5cfa887dea
{ashish} <{ashishsangwan26790@gmail.com}>
2025-06-22 20:21:30 +0530

    change dashboard.css name with temp.css again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import FeaturedPosts from '../components/HomePosts/FeaturedPosts';
 import TagScroller from '../components/Home/TagScroller';
 import Hero from '../components/Home/Hero';
-import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
   return (
